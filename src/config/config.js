f7f5687a630aa144fa87d663754eec948fc83733
sagarpatra07 <sagarpatra502@gmail.com>
2026-05-07 22:41:34 +0530

    Add Appwrite configuration and update dependencies

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+const config = {
+    appWriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
+    appWriteProjectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appWriteDbId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appWriteTableId : String(import.meta.env.VITE_APPWRITE_TABLE_ID),
+    appWriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+}
+
+export default config;
